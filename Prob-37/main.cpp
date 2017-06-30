18e2428d0824fc799175f04b2cd9b316536e5085
Tran Hau <hauvipapro@gmail.com>
2017-06-30 11:20:41 +0700

    Prob 37

---
+++ Prob-37/main.cpp
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int power(int a, int n)
+{
+	if(a == 0 || a == 1)
+		return a;
+	int result = 1;
+	for(int i = 0; i < n; i++)
+	{
+		result *= a;
+	}
+	return result;
+}
+
+int digit(int n)
+{
+	int temp = 0;
+	while(n > 0)
+	{
+		temp++;
+		n /= 10;
+	}
+	return temp;
+}
+
+bool isPrime(int n)
+{
+	if(n < 2)
+		return false;
+	if(n == 2)
+		return true;
+	if(n % 2 == 0)
+		return false;
+	for(int i = 3; i * i <= n; i += 2)
+	{
+		if(n % i == 0)
+			return false;
+	}
+	return true;
+}
+//The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right
+//and remain prime at each stage: 3797, 797, 97, and 7.
+//Similarly we can work from right to left: 3797, 379, 37, and 3.
+bool isTrunPrime(int n)
+{
+	int temp = n;
+	//from right
+	while(temp > 0)
+	{
+		if(!isPrime(temp))
+			return false;
+		temp /= 10;
+	}
+	temp = n;
+	//from left
+	while(temp > 0)
+	{
+		if(!isPrime(temp))
+			return false;
+		temp = temp % power(10, digit(temp) - 1);
+	}
+	return true;
+}
+
+// Erathos method to list prime number
+void Eratos(int MAX, int *&result, int &size)
+{
+	int *era = new int[MAX];
+	for(int i = 0; i < MAX; i++)
+	{
+		if(i == 0 || i == 1)
+			era[i] = 0;
+		else
+			era[i] = 1;
+	}
+	result = NULL;
+	size = 0;
+	for(int i = 2; i < MAX; i++)
+	{
+		if(era[i] == 1)
+		{
+			result = (int *)realloc(result, sizeof(int) * (size + 1));
+			result[size++] = i;
+			for(int j = 2; j <= MAX / i; j++)
+			{
+				era[i * j] = 0;
+			}
+		}
+	}
+	delete[]era;
+	era = NULL;
+}
+
+int sumTrunPrime(int MAX)
+{
+	int *result;
+	int size;
+	Eratos(MAX, result, size);
+	int sum = 0;
+	for(int i = 0; i < size; i++)
+	{
+		if(result[i] > 10)
+		{
+			if(isTrunPrime(result[i]))
+			{
+				//printf("%d\n", result[i]);
+				sum += result[i];
+			}
+		}
+	}
+	free(result);
+	result = NULL;
+	return sum;
+}
+
+int main()
+{
+	printf("%d\n", sumTrunPrime(1000000));
+	return 0;
+}
